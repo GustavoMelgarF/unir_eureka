ff189eaddcfbeb614ee334e44863d5313356d660
Gustavo Melgar <gustavomelgarf@gmail.com>
2025-06-07 17:43:49 +0200

    Initial Commit

---
+++ src/test/java/com/relatos_de_papel/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatos_de_papel.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
